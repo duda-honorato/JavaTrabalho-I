b6e8972fe6bccdd97efdffbb0de81d44fa1a413f
duda-honorato <111542543+duda-honorato@users.noreply.github.com>
2025-02-25 01:04:32 -0300

    Update Vendedor[1].java

--- Vendedor[1].java
+++ Vendedor[1].java
@@ -2,24 +2,25 @@ public class Vendedor extends Empregado {
     private double valorVendas;
     private double comissao;
 
-
-    public Vendedor(String nome, String telefone, int codigoSetor, double salarioBase, int imposto, double comissao, double valorVendas) {
+    public Vendedor(String nome, String telefone, int codigoSetor, double salarioBase, double imposto, double valorVendas, double comissao) {
         super(nome, telefone, codigoSetor, salarioBase, imposto);
-        if (valorVendas <  0.0) {
-            throw new IllegalArgumentException("Invalido");
+        if (valorVendas < 0.0) {
+            throw new IllegalArgumentException("Valor de vendas inválido");
         }
-        this.comissao = 10;
+        if (comissao < 0 || comissao > 100) {
+            throw new IllegalArgumentException("Comissão inválida");
+        }
         this.valorVendas = valorVendas;
+        this.comissao = comissao;
     }
-
 
     public double getValorVendas() {
         return valorVendas;
     }
 
     public void setValorVendas(double valorVendas) {
-        if (valorVendas <  0.0) {
-            throw new IllegalArgumentException("Invalido");
+        if (valorVendas < 0.0) {
+            throw new IllegalArgumentException("Valor de vendas inválido");
         }
         this.valorVendas = valorVendas;
     }
@@ -29,16 +30,21 @@ public class Vendedor extends Empregado {
     }
 
     public void setComissao(double comissao) {
+        if (comissao < 0 || comissao > 100) {
+            throw new IllegalArgumentException("Comissão inválida");
+        }
         this.comissao = comissao;
     }
+
     @Override
     public double calculaSalario() {
-        return super.calculaSalario() + (getValorVendas() * (getComissao()/100));
+        return super.calculaSalario() + (valorVendas * (comissao / 100));
     }
 
     @Override
     public String toString() {
-        return super.toString() +" valorVendas=" + valorVendas +
-                ", comissao=" + comissao + ", SalarioTotal= " + calculaSalario();
+        return super.toString() + ", Valor Vendas: " + valorVendas +
+                ", Comissão: " + comissao + "%" +
+                ", Salário Total: " + calculaSalario();
     }
 }
